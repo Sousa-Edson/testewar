f39a7ae0777882ac953d561dc9cdd49858b21287
Sousa-Edson <edson3711.es@gmail.com>
2023-03-08 07:19:18 -0300

    3 COMMIT

--- src/main/java/com/teste/app/TestewarApplication.java
+++ src/main/java/com/teste/app/TestewarApplication.java
@@ -2,13 +2,21 @@ package com.teste.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TestewarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestewarApplication.class, args);
 	}
-	//ssss
+
+	@GetMapping("/")
+	public String index() {
+		return "FUNCIONA AQUI";
+		
+	}
 
 }
