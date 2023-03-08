b4bd699356379e354e53ec0465a77a5decbb7049
Sousa-Edson <edson3711.es@gmail.com>
2023-03-08 01:19:20 -0300

    1 commit

---
+++ src/main/java/com/teste/app/TestewarApplication.java
@@ -0,0 +1,14 @@
+package com.teste.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestewarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestewarApplication.class, args);
+	}
+	//ssss
+
+}
